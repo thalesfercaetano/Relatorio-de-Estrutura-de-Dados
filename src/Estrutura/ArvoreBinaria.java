9437aff5f4e3175dfe6bd7cab960156689ea5b6e
Thales Caetano <thalesfercaetano@gmail.com>
2025-11-17 20:35:53 -0300

    feat: criacao de arvore binaria, insercao e busca

--- src/Estrutura/ArvoreBinaria.java
+++ src/Estrutura/ArvoreBinaria.java
@@ -1,5 +1,54 @@
 package Estrutura;
 
 public class ArvoreBinaria {
+    protected No raiz;
     
+    private static class No {
+        int valor;
+        No esquerda;
+        No direita;
+
+        No(int valor){
+            this.valor = valor;
+            this.esquerda = null;
+            this.direita = null;
+        }
+    }
+
+    public void inserir(int valor){
+        raiz = inserirRecursivo(raiz, valor);
+    }
+
+    protected No inserirRecursivo(No atual, int valor){
+        if(atual == null){
+            return new No(valor);
+        }
+        if(valor < atual.valor){
+            atual.esquerda = inserirRecursivo(atual.esquerda, valor);
+        }else if(valor > atual.valor){
+            atual.direita = inserirRecursivo(atual.direita, valor);
+        }
+        return atual;
+    }
+
+    public boolean buscar(int valor){
+        return buscarRecursivo(raiz, valor);
+    }
+
+    protected boolean buscarRecursivo(No atual, int valor){        
+        if(atual == null){
+            return false;
+        }
+        if(valor == atual.valor){
+            return true;
+        }
+        if(valor < atual.valor){
+            return buscarRecursivo(atual.esquerda, valor);
+        }
+        if(valor > atual.valor){
+            return buscarRecursivo(atual.direita, valor);
+        }
+
+        return false;
+    }
 }

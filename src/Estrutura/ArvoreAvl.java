8cdfe806edb0de5be0ccc25f7da4f7bd7e9a70f6
Thales Caetano <thalesfercaetano@gmail.com>
2025-11-24 13:23:08 -0300

    feat(ArvoreAVL): implementacao das rotacoes esquerda e direita

--- src/Estrutura/ArvoreAvl.java
+++ src/Estrutura/ArvoreAvl.java
@@ -42,4 +42,30 @@ public class ArvoreAvl {
         }
         return valor < atual.valor ? buscarRecursivo(atual.esquerda, valor) : buscarRecursivo(atual.direita, valor);
     }
+    
+    private No rotacaoDireita(No y) {
+        No x = y.esquerda;
+        No T2 = x.direita;
+
+        x.direita = y;
+        y.esquerda = T2;
+
+        y.altura = max(altura(y.esquerda), altura(y.direita)) + 1;
+        x.altura = max(altura(x.esquerda), altura(x.direita)) + 1;
+
+        return x;
+    }
+
+    private No rotacaoEsquerda(No x) {
+        No y = x.direita;
+        No T2 = y.esquerda;
+
+        y.esquerda = x;
+        x.direita = T2;
+
+        x.altura = max(altura(x.esquerda), altura(x.direita)) + 1;
+        y.altura = max(altura(y.esquerda), altura(y.direita)) + 1;
+
+        return y;
+    }    
 }

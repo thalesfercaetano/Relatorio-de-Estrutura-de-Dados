78e6177b7f3910cd4257faa280d41e6acaacc52c
Thales Caetano <thalesfercaetano@gmail.com>
2025-11-29 16:41:12 -0300

    feat: implementacao do metodo de gerar vetor inverso

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -11,6 +11,14 @@ public class Main{
         }
         return vetor;
     }
+
+    public static int[] gerarInverso(int tamanho){
+        int[] vetor = new int[tamanho];
+        for(int i = 0; i < tamanho; i++){
+            vetor[i] = tamanho - i - 1;
+        }
+        return vetor;
+    }
     public static void main(String[] args) {
 }
 }

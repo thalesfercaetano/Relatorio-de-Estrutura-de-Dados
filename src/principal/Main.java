6164917d272fbdbbb8e31e9088c131538092a7b4
Thales Caetano <thalesfercaetano@gmail.com>
2025-11-29 16:19:44 -0300

    feat: implementação do metodo para gerar vetor alternado

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,7 +1,16 @@
 package principal;
 
+import java.util.Random;
+
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static int[] gerarOrdenado(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = i;
+        }
+        return vetor;
     }
+    public static void main(String[] args) {
+}
 }

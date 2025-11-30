c48c3b45d37ef53dd60325f39218d21394e0f952
Thales Caetano <thalesfercaetano@gmail.com>
2025-11-30 10:48:54 -0300

    test: logica de medicao de tempo de insercao para as 3 estruturas

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -2,6 +2,10 @@ package principal;
 
 import java.util.Random;
 
+import Estrutura.ArvoreAvl;
+import Estrutura.ArvoreBinaria;
+import Estrutura.Vetor;
+
 public class Main{
 
     public static int[] gerarOrdenado(int tamanho) {
@@ -30,6 +34,48 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        System.err.println("hello");
-}
+        int[] tamanhos = {100, 1000, 10000};
+        String[] ordens = {"Ordenada", "Inversa", "Aleatória"};
+        
+        for (int tamanho : tamanhos) {
+            System.out.println("\n=== Tamanho: " + tamanho + " ===");
+            
+            for (String ordem : ordens) {
+                System.out.println("\n--- Ordem: " + ordem + " ---");
+                long tempoTotalInsercaoVetor = 0;
+                long tempoTotalInsercaoABB = 0;
+                long tempoTotalInsercaoAVL = 0;
+                
+                for (int i = 0; i < 5; i++) {
+                    int[] dados = switch (ordem) {
+                        case "Ordenada" -> gerarOrdenado(tamanho);
+                        case "Inversa" -> gerarInverso(tamanho);
+                        default -> gerarAleatorio(tamanho);
+                    };
+
+                    Vetor vetor = new Vetor(tamanho);
+                    long inicio = System.nanoTime();
+                    for (int v : dados) vetor.adicionar(v);
+                    long fim = System.nanoTime();
+                    tempoTotalInsercaoVetor += (fim - inicio);
+
+                    ArvoreBinaria abb = new ArvoreBinaria();
+                    inicio = System.nanoTime();
+                    for (int v : dados) abb.inserir(v);
+                    fim = System.nanoTime();
+                    tempoTotalInsercaoABB += (fim - inicio);
+
+                    ArvoreAvl avl = new ArvoreAvl();
+                    inicio = System.nanoTime();
+                    for (int v : dados) avl.inserir(v);
+                    fim = System.nanoTime();
+                    tempoTotalInsercaoAVL += (fim - inicio);
+                }
+
+                System.out.println("Média Inserção Vetor: " + (tempoTotalInsercaoVetor / 5) + " ns");
+                System.out.println("Média Inserção ABB: " + (tempoTotalInsercaoABB / 5) + " ns");
+                System.out.println("Média Inserção AVL: " + (tempoTotalInsercaoAVL / 5) + " ns");
+            }
+        }
+    }
 }

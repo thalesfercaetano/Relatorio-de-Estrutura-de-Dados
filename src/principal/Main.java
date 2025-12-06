798a7fca1b27e529712713300e5ecc257307aa3c
Thales Caetano <thalesfercaetano@gmail.com>
2025-12-06 09:42:08 -0300

    feat: implementacao dos testes de busca (primeiro, ultimo, meio, aleatorio)

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,50 +1,44 @@
 package principal;
 
 import java.util.Random;
-
 import Estrutura.ArvoreAvl;
 import Estrutura.ArvoreBinaria;
 import Estrutura.Vetor;
 
-public class Main{
+public class Main {
 
     public static int[] gerarOrdenado(int tamanho) {
         int[] vetor = new int[tamanho];
-        for (int i = 0; i < tamanho; i++) {
-            vetor[i] = i;
-        }
+        for (int i = 0; i < tamanho; i++) vetor[i] = i;
         return vetor;
     }
 
-    public static int[] gerarInverso(int tamanho){
+    public static int[] gerarInverso(int tamanho) {
         int[] vetor = new int[tamanho];
-        for(int i = 0; i < tamanho; i++){
-            vetor[i] = tamanho - i - 1;
-        }
+        for (int i = 0; i < tamanho; i++) vetor[i] = tamanho - i - 1;
         return vetor;
     }
 
     public static int[] gerarAleatorio(int tamanho) {
         int[] vetor = new int[tamanho];
         Random random = new Random();
-        for (int i = 0; i < tamanho; i++) {
-            vetor[i] = random.nextInt(tamanho * 10);
-        }
+        for (int i = 0; i < tamanho; i++) vetor[i] = random.nextInt(tamanho * 10);
         return vetor;
     }
 
     public static void main(String[] args) {
         int[] tamanhos = {100, 1000, 10000};
         String[] ordens = {"Ordenada", "Inversa", "Aleatória"};
-        
+        Random random = new Random();
+
         for (int tamanho : tamanhos) {
             System.out.println("\n=== Tamanho: " + tamanho + " ===");
-            
+
             for (String ordem : ordens) {
                 System.out.println("\n--- Ordem: " + ordem + " ---");
-                long tempoTotalInsercaoVetor = 0;
-                long tempoTotalInsercaoABB = 0;
-                long tempoTotalInsercaoAVL = 0;
+                
+                long tempoInsVetor = 0, tempoInsABB = 0, tempoInsAVL = 0;
+                long tempoBuscaVetor = 0, tempoBuscaABB = 0, tempoBuscaAVL = 0;
                 
                 for (int i = 0; i < 5; i++) {
                     int[] dados = switch (ordem) {
@@ -54,27 +48,48 @@ public class Main{
                     };
 
                     Vetor vetor = new Vetor(tamanho);
+                    ArvoreBinaria abb = new ArvoreBinaria();
+                    ArvoreAvl avl = new ArvoreAvl();
+
                     long inicio = System.nanoTime();
                     for (int v : dados) vetor.adicionar(v);
-                    long fim = System.nanoTime();
-                    tempoTotalInsercaoVetor += (fim - inicio);
+                    tempoInsVetor += (System.nanoTime() - inicio);
 
-                    ArvoreBinaria abb = new ArvoreBinaria();
                     inicio = System.nanoTime();
                     for (int v : dados) abb.inserir(v);
-                    fim = System.nanoTime();
-                    tempoTotalInsercaoABB += (fim - inicio);
+                    tempoInsABB += (System.nanoTime() - inicio);
 
-                    ArvoreAvl avl = new ArvoreAvl();
                     inicio = System.nanoTime();
                     for (int v : dados) avl.inserir(v);
-                    fim = System.nanoTime();
-                    tempoTotalInsercaoAVL += (fim - inicio);
+                    tempoInsAVL += (System.nanoTime() - inicio);
+                    
+                    int[] alvos = {
+                        dados[0], 
+                        dados[tamanho - 1], 
+                        dados[tamanho / 2], 
+                        dados[random.nextInt(tamanho)], 
+                        dados[random.nextInt(tamanho)], 
+                        dados[random.nextInt(tamanho)], 
+                        -1 
+                    };
+
+                    for (int alvo : alvos) {
+                        inicio = System.nanoTime(); 
+                        vetor.buscaSequencial(alvo); 
+                        tempoBuscaVetor += (System.nanoTime() - inicio);
+
+                        inicio = System.nanoTime(); 
+                        abb.buscar(alvo); 
+                        tempoBuscaABB += (System.nanoTime() - inicio);
+
+                        inicio = System.nanoTime(); 
+                        avl.buscar(alvo); 
+                        tempoBuscaAVL += (System.nanoTime() - inicio);
+                    }
                 }
 
-                System.out.println("Média Inserção Vetor: " + (tempoTotalInsercaoVetor / 5) + " ns");
-                System.out.println("Média Inserção ABB: " + (tempoTotalInsercaoABB / 5) + " ns");
-                System.out.println("Média Inserção AVL: " + (tempoTotalInsercaoAVL / 5) + " ns");
+                System.out.println("Inserção Média (ns) -> Vetor: " + (tempoInsVetor/5) + " | ABB: " + (tempoInsABB/5) + " | AVL: " + (tempoInsAVL/5));
+                System.out.println("Busca Média (ns)    -> Vetor: " + (tempoBuscaVetor/35) + " | ABB: " + (tempoBuscaABB/35) + " | AVL: " + (tempoBuscaAVL/35));
             }
         }
     }
